6beee8e0b1493d57538f273f945920fb3d79276b
Beau Kinstler <git@beaukinstler.info>
2016-04-04 23:53:16 -0500

    this has good tweaks I made on the tutorial's degugging tips, including a merge of some of Michael Hartl's debugging tips

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,16 @@
 class VisitorsController < ApplicationController
 
   def new
+    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
+    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
+    
+    # for dumping the debug to the view in development
+    if Rails.env.development?
+      @debug = 'DEBUG: entering new method'
+      @debug = @debug + "\n"+'DEBUG: Owner name is ' + @owner.name
+    end
   end
 
 end
