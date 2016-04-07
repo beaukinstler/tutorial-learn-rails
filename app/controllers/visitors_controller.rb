ea500b786cec000826f7ffb0500b6c16f6d0b5e1
Beau Kinstler <git@beaukinstler.info>
2016-04-07 14:03:15 -0500

    Experimenting with Layout gem and flash messages, including message when in dev envrionment

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -6,11 +6,16 @@ class VisitorsController < ApplicationController
     Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
     Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
     
+
+    flash[:notice] = 'Welcome!'    
     # for dumping the debug to the view in development
     if Rails.env.development?
       @debug = 'DEBUG: entering new method'
       @debug = @debug + "\n"+'DEBUG: Owner name is ' + @owner.name
+      flash[:notice] = "#{flash[:notice]} - DEVELOPMENT"
     end
+
+    flash[:alert] = 'My birthday is soon.'
   end
 
 end
