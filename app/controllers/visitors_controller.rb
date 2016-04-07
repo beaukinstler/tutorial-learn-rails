b4ab219d6fbc541d692f04c7375024a903015507
Beau Kinstler <git@beaukinstler.info>
2016-04-07 15:34:05 -0500

    Adding front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,21 +1,6 @@
 class VisitorsController < ApplicationController
 
   def new
-    Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-    Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
-    
-
-    flash[:notice] = 'Welcome!'    
-    # for dumping the debug to the view in development
-    if Rails.env.development?
-      @debug = 'DEBUG: entering new method'
-      @debug = @debug + "\n"+'DEBUG: Owner name is ' + @owner.name
-      flash[:notice] = "#{flash[:notice]} - DEVELOPMENT"
-    end
-
-    flash[:alert] = 'My birthday is soon.'
   end
-
 end
